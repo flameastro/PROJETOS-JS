8d337e70ecb5ca84d4660a2c44995e3244b081e5
flameastro <gabrielgoulartbnu@gmail.com>
2026-06-18 15:24:26 -0300

    feat: adiciona categoria

--- bookmark-saver/scripts/script.js
+++ bookmark-saver/scripts/script.js
@@ -12,12 +12,30 @@ function getPreviousBookmarks() {
             bookmark.id = id
             bookmarks.push(bookmark)
 
-            bookmarksSection.innerHTML += `
-                <div id="${id}">
-                    <span><a href="${bookmark.url}" target="_blank">${bookmark.name}</a></span>
-                    <button type="button" class="delete btn btn-danger">Delete</button>
-                </div>
-            `
+            let categoryExists = false
+            for (let article of bookmarksSection.children) {
+                if (article.getAttribute("class") == bookmark.category) {
+                    categoryExists = true
+                    article.innerHTML += `
+                        <div id="${bookmark.id}">
+                            <span><a href="${bookmark.url}" target="_blank">${bookmark.name}</a></span>
+                            <button type="button" class="delete btn btn-danger">Delete</button>
+                        </div>
+                    `
+                }
+            }
+
+            if (!categoryExists) {
+                bookmarksSection.innerHTML += `
+                    <article class="${bookmark.category}">
+                        <h2>${bookmark.category}</h2>
+                        <div id="${bookmark.id}">
+                            <span><a href="${bookmark.url}" target="_blank">${bookmark.name}</a></span>
+                            <button type="button" class="delete btn btn-danger">Delete</button>
+                        </div>
+                    </article>
+                `
+            }
 
             id++
         }
@@ -28,31 +46,48 @@ getPreviousBookmarks()
 
 
 function addBookmark() {
+    const category = document.querySelector("#category").value
     const name = document.querySelector("#bookmark").value
     const url = document.querySelector("#url").value
     let id = bookmarksSection.childElementCount+1
+    let categoryExists = false
 
-    if ((name.length > 0 && name.length <= 50) && (url.length > 0)) {
-        bookmarksSection.innerHTML += `
-            <div id="${id}">
-                <span><a href="${url}" target="_blank">${name}</a></span>
-                <button type="button" class="delete btn btn-danger">Delete</button>
-            </div>
-        `
-
-        let newBookmark = {
-            "id": id,
-            "name": name,
-            "url": url
+    for (let article of bookmarksSection.children) {
+        if (article.getAttribute("class").toLowerCase() == category.toLowerCase()) {
+            categoryExists = true
+            article.innerHTML += `
+                <div id="${id}">
+                    <span><a href="${url}" target="_blank">${name}</a></span>
+                    <button type="button" class="delete btn btn-danger">Delete</button>
+                </div>
+            `
         }
-
-        bookmarks.push(newBookmark)
-        localStorage.setItem("bookmarks", JSON.stringify(bookmarks))
-
-        deleteBookmark()
-    } else {
-        alert("The name length need to be greather than 0 and less or equal than 50")
     }
+
+    if (!categoryExists) {
+        bookmarksSection.innerHTML += `
+            <article class="${category.toLowerCase()}">
+                <h2>${category.toLowerCase()}</h2>
+
+                <div id="${id}">
+                    <span><a href="${url}" target="_blank">${name}</a></span>
+                    <button type="button" class="delete btn btn-danger">Delete</button>
+                </div>
+            </article>
+        `
+    }
+
+    let newBookmark = {
+        "id": id,
+        "category": category.toLowerCase(),
+        "name": name,
+        "url": url
+    }
+
+    bookmarks.push(newBookmark)
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks))
+
+    deleteBookmark()
 }
 
 
